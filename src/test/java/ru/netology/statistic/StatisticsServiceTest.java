110e3ee73d75996c8fd9b2e6d02fd096eb05f05f
Neto Georgiy <dirtyhnkt@gmail.com>
2025-12-23 20:47:15 +0300

    Add tests for 100% coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,29 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax_singleElement() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {42};
+        long actual = service.findMax(incomes);
+        Assertions.assertEquals(42, actual);
+    }
+
+    @Test
+    void findMax_allSameValues() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {5, 5, 5, 5};
+        long actual = service.findMax(incomes);
+        Assertions.assertEquals(5, actual);
+    }
+
+    @Test
+    void findMax_negativeNumbers() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {-10, -5, -20, -1};
+        long actual = service.findMax(incomes);
+        Assertions.assertEquals(-1, actual);
+    }
+
 }
